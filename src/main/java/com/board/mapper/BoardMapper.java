7512a9ccef3462867db9cbc16707acbbccc3b850
박세빈 <tpqls4105@gmail.com>
2024-03-27 17:31:54 +0900

    03-27 퇴근

---
+++ src/main/java/com/board/mapper/BoardMapper.java
@@ -0,0 +1,21 @@
+package com.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.board.domain.BoardVo;
+import com.board.menus.domain.MenuVo;
+
+@Mapper
+public interface BoardMapper {
+
+	List<BoardVo> getBoardList(MenuVo menuVo);
+
+	void insertBoard(BoardVo boardVo);
+
+	BoardVo getBoard(BoardVo boardVo);
+
+	void incHit(BoardVo boardVo);
+
+}
